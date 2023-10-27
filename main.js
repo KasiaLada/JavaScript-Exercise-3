6fb97ee4dae822bbc361456c4a43479dad7f6db3
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2023-10-27 13:14:50 +0200

    Exercise done

---
+++ main.js
@@ -0,0 +1,16 @@
+const arrowIcon = document.querySelector('.fas');
+const btn = document.querySelector('.arrow');
+const img = document.querySelector('.item1');
+
+function showImage() {
+	img.classList.toggle('show');
+	if (img.classList.contains('show')) {
+		// arrowIcon.style.rotate = '180deg';
+		arrowIcon.style.transform = 'rotate(180deg)';
+	} else {
+		// arrowIcon.style.rotate = '0deg';
+		arrowIcon.style.transform = 'rotate(0deg)';
+	}
+}
+
+btn.addEventListener('click', showImage);
